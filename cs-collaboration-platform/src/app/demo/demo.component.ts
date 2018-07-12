83672316d1f2f4f9b3ea21b3bfd46e47d2b50ddf
teamdelta2018 <franklinpracticum2018@gmail.com>
2018-07-11 22:29:12 -0400

    demo returns array of user objects.
    Signed-off-by: teamdelta2018 <franklinpracticum2018@gmail.com>

--- cs-collaboration-platform/src/app/demo/demo.component.ts
+++ cs-collaboration-platform/src/app/demo/demo.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from 'angularfire2/firestore';
-import { Observable } from 'rxjs';
+import { Observable , Subject, } from 'rxjs';
+import {switchMap} from 'rxjs/operators';
 @Component({
   selector: 'app-demo',
   templateUrl: './demo.component.html',
@@ -9,11 +10,35 @@ import { Observable } from 'rxjs';
 export class DemoComponent implements OnInit {
   items: Observable<any[]>;
   current: Observable<any[]>;
+  public data: Array<any> =[];
+  database: AngularFirestore;
   constructor(db: AngularFirestore) { 
-    this.items = db.collection('users').valueChanges();
-    this.current = db.collection('users', ref => ref.where('email', '==', 'franklinpracticum2018@gmail.com')).valueChanges();
+
+   
+
+  this.items = db.collection('users').valueChanges();
+	this.database = db;
+   
   }
 
+  checkForUser(user: string) {
+	  console.log("check user got: ", user);
+    this.current = this.database.collection('users').valueChanges();
+    this.current.subscribe(data=>{
+      console.log(data)
+      data.forEach(function (value) {
+        console.log(value.username);
+        if (value.username == user) {
+          console.log("user found")
+          return true;
+        }
+      });
+      
+    });
+    
+  }
+
+  
   ngOnInit() {
   }
 
