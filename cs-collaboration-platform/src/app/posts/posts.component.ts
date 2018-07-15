918e668995ea2909661789a07056f25bb581a96c
teamdelta2018 <franklinpracticum2018@gmail.com>
2018-07-15 19:35:30 -0400

    Added functionality to display posts and responses from only the logged in user.
    Signed-off-by: teamdelta2018 <franklinpracticum2018@gmail.com>

--- cs-collaboration-platform/src/app/posts/posts.component.ts
+++ cs-collaboration-platform/src/app/posts/posts.component.ts
@@ -11,13 +11,27 @@ import { Observable , Subject, } from 'rxjs';
 export class PostsComponent implements OnInit {
   items: Observable<any[]>;
   responses: Observable<any[]>;
+  test: Observable<any[]>;
   database: AngularFirestore;
   constructor(public authService: AuthService, public db: AngularFirestore) {
     this.items = db.collection('posts').valueChanges();
     this.responses = db.collection('responses').valueChanges();
-	  this.database = db;
+    this.database = db;
+    this.responses = db.collection('responses').valueChanges();
+    this.test = db.collection('posts').valueChanges();
+    this.test.subscribe(data=> {
+      console.log("posts", data);
+    });
    }
-
+ 
+  contains (id: string, user: string ) {
+    console.log("id passed to contains is ", id);
+    console.log("user passed to contains is ", user);
+    //this.authService.user.subscribe(data => console.log(data));
+    console.log(id, " includes ", user, " : ", id.includes(user))
+    return id.includes(user.split("@")[0]);
+  }
+   
   ngOnInit() {
   }
 
